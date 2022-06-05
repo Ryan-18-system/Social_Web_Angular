07bcaf00d9f1e508dc31cb1e889ee074395fdf2f
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-06-05 20:50:13 -0300

    first commit

---
+++ src/app/usuario/listagem-usuario/listagem-usuario.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {Usuario} from '../../shared/modelo/usuario';
+import {UsuarioService} from '../../shared/servicos/usuario.service';
+
+@Component({
+  selector: 'app-listagem-usuario',
+  templateUrl: './listagem-usuario.component.html',
+  styleUrls: ['./listagem-usuario.component.css']
+})
+export class ListagemUsuarioComponent implements OnInit {
+
+  usuarios: Usuario[];
+  constructor(private usuarioService: UsuarioService) {
+    this.usuarios = new Array<Usuario>();
+  }
+
+  ngOnInit(): void {
+    this.usuarioService.listar().subscribe(
+      usuarios => this.usuarios = usuarios
+    );
+  }
+
+  removerUsuario(usuario: Usuario) {
+    this.usuarioService.remover(usuario.id || 0).subscribe(
+      removido => {
+        const indxUsuario = this.usuarios.findIndex(u => u.id === usuario.id);
+
+        if (indxUsuario > -1) {
+          this.usuarios.splice(indxUsuario, 1);
+        }
+      }
+    );
+  }
+
+  ehMaiorDeIdade(usuario: Usuario): boolean {
+    return (usuario.idade || 0) >= 18;
+  }
+
+  quantidadeUsuariosMaiorDeIdade(): number {
+    return this.usuarios.filter(usuario => this.ehMaiorDeIdade(usuario)).length;
+  }
+
+}
